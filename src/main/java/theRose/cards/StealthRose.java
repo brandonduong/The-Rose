7a42faf42ef94e2a186307a827e5427a370d16f4
Brandon Duong <brandon.duong@outlook.com>
2020-04-30 16:48:29 -0400

    Added new Stealth Rose card.

---
+++ src/main/java/theRose/cards/StealthRose.java
@@ -0,0 +1,78 @@
+package theRose.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+import theRose.ModInitializer;
+import theRose.actions.RandomFoodConsumeAction;
+import theRose.characters.TheRose;
+
+import static theRose.ModInitializer.makeCardPath;
+
+public class StealthRose extends AbstractDynamicCard {
+
+    /*
+     * Stealth Rose: Gain !B! Block. Draw !M! card. This is unaffected by Flight.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = ModInitializer.makeID(StealthRose.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+    // Upgraded description
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheRose.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 4;
+    private static final int UPGRADE_BLOCK = 3;
+    private static final int DRAW = 1;
+
+    // /STAT DECLARATION/
+
+    public StealthRose() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = DRAW;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // Gain block
+        if (!upgraded) {
+            this.addToBot(new GainBlockAction(p, BLOCK));
+        }
+        else {
+            this.addToBot(new GainBlockAction(p, BLOCK + UPGRADE_BLOCK));
+        }
+        
+        // Draw card
+        this.addToBot(new DrawCardAction(DRAW));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
