c4b1487f924029c592f7733fd40195f7e4794343
Brandon Duong <brandon.duong@outlook.com>
2020-05-17 19:10:46 -0400

    Added new Bomb card.

---
+++ src/main/java/theRose/cards/Bomb.java
@@ -0,0 +1,70 @@
+package theRose.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRose.ModInitializer;
+import theRose.characters.TheRose;
+
+import static theRose.ModInitializer.makeCardPath;
+
+public class Bomb extends AbstractDynamicCard {
+
+    /*
+     * Agent Rose's Bomb: Whenever you draw this card, reduce its cost by !M!. Deal !D! to all enemies.
+     *                    Play 1 Mission Accomplished. Exhaust.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = ModInitializer.makeID(Bomb.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheRose.Enums.COLOR_GRAY;
+
+    private static final int COST = 12;
+    private static final int DAMAGE = 100;
+    private static final int REDUCE = 1;
+
+    // /STAT DECLARATION/
+
+    public Bomb() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = REDUCE;
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        this.addToBot(new ReduceCostAction(this.uuid, this.magicNumber));
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // Deal damage
+        this.addToBot(new DamageAllEnemiesAction(p, baseDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            initializeDescription();
+        }
+    }
+}
