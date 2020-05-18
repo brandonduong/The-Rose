80e4103996966c2fca25e0b87d67afa4ab436ace
Brandon Duong <brandon.duong@outlook.com>
2020-05-18 19:47:59 -0400

    Reworked Early Bird to deal damage equal to 2 times current flight.

--- src/main/java/theRose/cards/EarlyBird.java
+++ src/main/java/theRose/cards/EarlyBird.java
@@ -1,6 +1,9 @@
 package theRose.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -15,7 +18,7 @@ import static theRose.ModInitializer.makeCardPath;
 public class EarlyBird extends AbstractDynamicCard {
 
     /*
-     * Early Bird: Gain !M! Flight. Add 1 random Food items to your hand.
+     * Early Bird: Gain !M! Flight. Deal damage equal to 2 times your current Flight. Add a random Food item to your hand.
      */
 
     // TEXT DECLARATION
@@ -32,21 +35,22 @@ public class EarlyBird extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheRose.Enums.COLOR_GRAY;
 
-    private static final int COST = 0;
-    private static final int BUFF = 1; // Give 1 Penguin Flight
+    private static final int COST = 1;
+    private static final int BUFF = 2; // Give 1 Penguin Flight
     private static final int CREATE = 1; // Create random food items
     private static final int UPGRADE_BUFF = 1;
+    private static final int MULTIPLIER = 2;
 
 
     // /STAT DECLARATION/
 
     public EarlyBird() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseMagicNumber = BUFF;
+        baseMagicNumber = magicNumber = BUFF;
         BaseSecondMagicNumber = SecondMagicNumber = CREATE;
 
     }
@@ -54,8 +58,18 @@ public class EarlyBird extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        // Deal damage
+        // Only apply if in flight
+        if (p.hasPower("Flight")) {
+            baseDamage = damage = p.getPower("Flight").amount * MULTIPLIER;
+
+            // Deal damage
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage,
+                    damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        }
+
         // Gain flight (Penguin Power)
-        this.addToBot(new ApplyPowerAction(p, p, new PenguinPower(p, baseMagicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new PenguinPower(p, magicNumber)));
 
         // Create random food items
         for (int i = 0; i < SecondMagicNumber; i++) {
