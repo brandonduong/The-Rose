985dee4ab591feb383895c842cd132743dcf908a
Brandon Duong <brandon.duong@outlook.com>
2020-05-17 20:43:09 -0400

    Fixed choice bug

--- src/main/java/theRose/actions/PickyEaterAction.java
+++ src/main/java/theRose/actions/PickyEaterAction.java
@@ -1,28 +1,21 @@
 package theRose.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.screens.CardRewardScreen;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToDiscardEffect;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
-import theRose.ModInitializer;
-import theRose.cards.*;
 
 import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class PickyEaterAction extends AbstractGameAction {
     private AbstractPlayer p;
     private boolean upgrade;
-    private boolean retrieveCard = false;
 
-    private final ArrayList<AbstractCard> foodChoices;
+    private final ArrayList<AbstractCard> choices;
 
-    public PickyEaterAction(boolean upgrade, ArrayList<AbstractCard> foodChoices) {
+    public PickyEaterAction(boolean upgrade, ArrayList<AbstractCard> choices) {
         this.upgrade = upgrade;
 
         this.p = AbstractDungeon.player;
@@ -31,36 +24,42 @@ public class PickyEaterAction extends AbstractGameAction {
 
         this.actionType = ActionType.CARD_MANIPULATION;
 
-        this.foodChoices = foodChoices;
+        this.choices = choices;
     }
 
 
     public void update() {
-        if (this.duration == Settings.ACTION_DUR_FAST) {
-            AbstractDungeon.cardRewardScreen.customCombatOpen(this.foodChoices, CardRewardScreen.TEXT[1], false);
-        }
-        else {
-            if (!this.retrieveCard) {
-                if (AbstractDungeon.cardRewardScreen.discoveryCard != null) {
-                    AbstractCard disCard = AbstractDungeon.cardRewardScreen.discoveryCard.makeStatEquivalentCopy();
-                    if (this.upgrade) {
-                        disCard.upgrade();
-                    }
+        CardGroup tmp;
+        if (this.duration == Settings.ACTION_DUR_MED){
+            tmp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
 
-                    disCard.current_x = -1000.0F * Settings.scale;
-                    if (AbstractDungeon.player.hand.size() < 10) {
-                        AbstractDungeon.effectList.add(new ShowCardAndAddToHandEffect(disCard, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
-                    } else {
-                        AbstractDungeon.effectList.add(new ShowCardAndAddToDiscardEffect(disCard, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
-                    }
-
-                    AbstractDungeon.cardRewardScreen.discoveryCard = null;
+            for (AbstractCard c : this.choices) {
+                if (this.upgrade) {
+                    c.upgrade();
                 }
-
-                this.retrieveCard = true;
+                tmp.addToTop(c);
             }
 
+            AbstractDungeon.gridSelectScreen.open(tmp, 1, "Choose", false);
+            tickDuration();
+            return;
         }
-        this.tickDuration();
+        if (AbstractDungeon.gridSelectScreen.selectedCards.size() != 0){
+            for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards){
+                c.unhover();
+                if (this.p.hand.size() == 10){
+                    this.p.createHandIsFullDialog();
+                    this.p.discardPile.addToTop(c);
+                }
+                else{
+                    this.p.hand.addToTop(c);
+                }
+                this.p.hand.refreshHandLayout();
+                this.p.hand.applyPowers();
+            }
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
+            this.p.hand.refreshHandLayout();
+        }
+        tickDuration();
     }
 }
