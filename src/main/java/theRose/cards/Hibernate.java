813916b5479ca9d3285234c8a856774e1bfaa674
Brandon Duong <brandon.duong@outlook.com>
2020-05-19 19:57:35 -0400

    Buffed Hibernate to consume 3 Food items.

--- src/main/java/theRose/cards/Hibernate.java
+++ src/main/java/theRose/cards/Hibernate.java
@@ -44,7 +44,7 @@ public class Hibernate extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int BLOCK = 6;
-    private static final int CREATE = 1;
+    private static final int CREATE = 3;
     private static final int ENERGY = 1;
     private static final int UPGRADE_ENERGY = 1;
 
