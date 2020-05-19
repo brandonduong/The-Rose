8e9f0cc3a2556dc53b369728eb779ae0e4c2c15f
Brandon Duong <brandon.duong@outlook.com>
2020-05-18 21:15:37 -0400

    Reworked Picky Eater to be 1-cost attack.

--- src/main/java/theRose/cards/PickyEater.java
+++ src/main/java/theRose/cards/PickyEater.java
@@ -1,6 +1,9 @@
 package theRose.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -35,12 +38,12 @@ public class PickyEater extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheRose.Enums.COLOR_GRAY;
 
-    private static final int COST = 0;
-
+    private static final int COST = 1;
+    private static final int DAMAGE = 5;
     private static final int CHOOSE = 1; // How many random food cards to create
 
     // /STAT DECLARATION/
@@ -48,11 +51,15 @@ public class PickyEater extends AbstractDynamicCard {
     public PickyEater() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = CHOOSE;
+        baseDamage = DAMAGE;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        // Deal damage
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+
         // Create list of choices
         ArrayList<AbstractCard> foodChoices = new ArrayList();
         foodChoices.add(new Donut());
